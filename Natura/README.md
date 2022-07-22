d3ad18b2d381c79fe29150c6fe32505460a812d0
saraimenco <saraimencod@gmail.com>
2022-07-21 22:23:08 -0500

    Landing de blog

---
+++ Natura/README.md
@@ -0,0 +1,25 @@
+# Proyecto final Bootcamp.
+
+
+
+# Tecnologías utilizadas.
+- HTML
+- CSS
+- ANGULAR
+- Typescript
+- Javascript
+- Node Js
+- MONGO DB
+
+
+
+#Integrantes:
+
+*Sarai Menco
+*Juan Camilo
+*Frank Edier Gonzales
+*Ivan Andres Trujillo gutiérrez
+
+## Link del proyecto.
+(https://github.com/ivantrugu/horizon.git)
+
---
+++ Natura/src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+
+
+import { LoginComponent } from './component/login/login.component';
+import { CarruselComponent } from './component/carrusel/carrusel.component';
+
+import { HomeComponent } from './component/home/home.component';
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+
+    CarruselComponent,
+
+    LoginComponent,
+    HomeComponent,
+ 
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
